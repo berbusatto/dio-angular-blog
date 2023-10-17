b5445e27eaad789f76ec5d6aef0735aa45a967f8
Bernardo Busatto <berbusatto@gmail.com>
2023-10-17 15:04:39 -0300

    add dinamyc data on components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,23 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://sm.ign.com/t/ign_br/screenshot/default/imagem-2023-08-08-120220091_74cs.960.jpg",
+        "title": "Magic: The Gathering ganhará cartas em parceria com franquias famosas dos games",
+        "description": "O famoso card game, Magic: The Gathering, anunciou novidades que deixarão os gamers de boca aberta! A Wizard of the Coast, empresa produtora do jogo, anunciou uma parceria com Final Fantasy, Fallout e Assassin's Creed para o próximo ano, 2024. As novas cartas farão parte da coleção Universes Beyond. O primeiro set a ser lançado é da icônica franquia da Bethesda, Fallout, seguida pelo pack da irmandade dos assassinos. Final Fantasy, por sua vez, não recebeu data de lançamento, mas chegará em 2024. Cada carta contará com elementos únicos das franquias, por exemplo, AC contará com mecânicas únicas baseadas na franquia. Já Final Fantasy terá cartas baseadas em cada jogo da franquia incluindo FF16, e Fallout contará com localizações pós-apocalíptica."
+    },
+    {
+        "id": "2",
+        "photo": "https://sm.ign.com/t/ign_br/screenshot/default/magic-senhor-dos-aneis_8a9b.960.jpg",
+        "title": "Jogador que vendeu carta 'Um Anel' de Magic por mais de US$ 2 milhões revela o que fez com o dinheiro",
+        "description": "O famoso caso da carta 'O Anel' de Magic: The Gathering em colaboração com a franquia O Senhor dos Anéis ganhou mais um capítulo. Para quem não sabe, um sortudo encontrou a raridade após comprar uma caixa de boosters em Toronto, no Canadá. Depois de levar para a empresa de avaliações PSA, Brook Trafton (o sortudo) começou a receber várias propostas de compra. No final, ele acabou vendendo para o rapper Post Malone por US$ 2,6 milhões (mais de R$ 12,6 milhões) - sim, Malone é um fã obcecado de Magic. Agora, Trafton conta o que fez com o dinheiro."
+    },
+    
+    {
+        "id": "3",
+        "photo": "https://sm.ign.com/t/ign_br/screenshot/default/en-vhpxxvjawo_gs7a.960.png",
+        "title": "Conheça as cartas e mecânicas de cada deck da coleção de Doctor Who em Magic: The Gathering",
+        "description": "Se você é fã de Doctor Who e Magic: The Gathering, prepare-se para a coleção inspirada na clássica série da TV britânica que será lançada em 13 de outubro. Com muitos anos e episódios de história para condensar em uma única coleção, a Wizards of the Coast decidiu lançar quatro decks do tipo Commander, já prontos para jogar, além de boosters de colecionador, com cartas ainda mais especiais. "
+    }   
+
+
+]
